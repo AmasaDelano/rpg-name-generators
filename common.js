6ded1dd291d0f03f9f6fcc96d84491e02f30ca1c
Alex Leone <me@alexjleone.com>
2019-09-13 03:18:37 +0930

    Adding underscore to fix shuffle function; DRYing

---
+++ common.js
@@ -0,0 +1,22 @@
+(function () {
+
+    // Pick a random number of items from a list.
+    window.pickRandomFromList = function pickRandomFromList(list, number) {
+        var randomizedList = _.shuffle(list);
+        return randomizedList[number || 0];
+    }
+
+    // Return an array of the selected opion values
+    // select is an HTML select element
+    window.getSelectValues = function getSelectValues(select) {
+        var options = select && select.options;
+
+        var result = [];
+        for (var option of options) {
+            if (option.selected) {
+                result.push(option.value || option.text);
+            }
+        }
+        return result;
+    }
+}());
